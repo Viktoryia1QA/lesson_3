541f7f53241b0abee4386e617036d955604599a7
Viktoryia1QA <ViktoryiaAntonchyk@gmail.com>
2022-02-07 01:41:50 +0300

    save changes

--- lesson_3.js
+++ lesson_3.js
@@ -1,1 +1,15 @@
-console.log("hi! world!");
+const fse = require('fs-extra');
+
+fse.ensureDirSync('./firstFolder');
+fse.ensureDirSync('./secondFolder');
+
+fse.ensureFileSync('./firstFolder/file.txt');
+
+fse.moveSync('./firstFolder/file.txt', './secondFolder/file.txt');
+
+fse.removeSync('./secondFolder/file.txt');
+
+fse.removeSync('./firstFolder');
+fse.removeSync('./secondFolder');
+
+console.log('lesson 3 is finished');
